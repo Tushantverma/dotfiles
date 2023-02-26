1303e0bf2cd4f60a0a22e42a46492cc45f092e63
Your Name <you@example.com>
2023-02-26 22:56:00 +0530

    added my setup all script

---
+++ dot_myscripts/executable_setup_all.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# building dwm
+cd ~/.config/tv-dwm/chadwm/
+sudo make
+sudo make install
+sudo make clean
+
+# building st
+cd ~/.config/tv-dwm/st/
+sudo make
+sudo make install
+sudo make clean
+
+# installing vibraint linux
+cd ~/.myscripts/
+bash ./setup-libvibrant-pwd.sh
+
