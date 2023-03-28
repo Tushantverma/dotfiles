8427e61ac82792824848846e461d85dc31d7f6a0
Tushantverma <108903932+Tushantverma@users.noreply.github.com>
2023-03-29 00:01:04 +0530

    Commented update module, just update

--- private_dot_config/tv-dwm/scripts/executable_bar.sh
+++ private_dot_config/tv-dwm/scripts/executable_bar.sh
@@ -54,8 +54,8 @@ clock() {
 
 while true; do
 
-  [ $interval = 0 ] || [ $(($interval % 3600)) = 0 ] && updates=$(pkg_updates)
-  interval=$((interval + 1))
+  #[ $interval = 0 ] || [ $(($interval % 3600)) = 0 ] && updates=$(pkg_updates)
+  #interval=$((interval + 1))
 
   xsetroot -name "$(battery) $(brightness) $(cpu) $(mem) $(clock)" && sleep 5 
 done
