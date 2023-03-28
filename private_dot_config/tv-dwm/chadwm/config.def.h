cdc7347e8a90745b206deadc46512b29736e5bfe
Tushantverma <Tushantverma@users.noreply.github.com>
2023-03-28 20:10:42 +0530

    added brightness and volume key function in sxhkdrc

--- private_dot_config/tv-dwm/chadwm/config.def.h
+++ private_dot_config/tv-dwm/chadwm/config.def.h
@@ -28,11 +28,14 @@ static const int horizpadtabo       = 15;
 static const int scalepreview       = 4;
 static const int tag_preview        = 1;        /* 1 means enable, 0 is off */
 static const int colorfultag        = 1;        /* 0 means use SchemeSel for selected non vacant tag */
-static const char *upvol[]   = { "/usr/bin/pactl", "set-sink-volume", "0", "+5%",     NULL };
-static const char *downvol[] = { "/usr/bin/pactl", "set-sink-volume", "0", "-5%",     NULL };
-static const char *mutevol[] = { "/usr/bin/pactl", "set-sink-mute",   "0", "toggle",  NULL };
-static const char *light_up[] = {"/usr/bin/light", "-A", "5", NULL};
-static const char *light_down[] = {"/usr/bin/light", "-U", "5", NULL};
+
+// ### adding volume and brightness keys in sxhkd to be independent form any window manager ### || keybinding defined billow//
+//static const char *upvol[]   = { "/usr/bin/pactl", "set-sink-volume", "0", "+5%",     NULL };
+//static const char *downvol[] = { "/usr/bin/pactl", "set-sink-volume", "0", "-5%",     NULL };
+//static const char *mutevol[] = { "/usr/bin/pactl", "set-sink-mute",   "0", "toggle",  NULL };
+//static const char *light_up[] = {"/usr/bin/light", "-A", "5", NULL};
+//static const char *light_down[] = {"/usr/bin/light", "-U", "5", NULL};
+
 static const int new_window_attach_on_end = 0; /*  1 means the new window will attach on the end; 0 means the new window will attach on the front,default is front */
 #define ICONSIZE 19   /* icon size */
 #define ICONSPACING 8 /* space between icon and title */
@@ -159,12 +162,12 @@ static const Layout layouts[] = {
 static const Key keys[] = {
     /* modifier                         key                 function        argument */
 
-    // brightness and audio 
-    {0,                    XF86XK_AudioLowerVolume,        spawn,        {.v = downvol}},
-	{0,                    XF86XK_AudioMute,               spawn,        {.v = mutevol }},
-	{0,                    XF86XK_AudioRaiseVolume,        spawn,        {.v = upvol}},
-	{0,                    XF86XK_MonBrightnessUp,         spawn,	     {.v = light_up}},
-	{0,                    XF86XK_MonBrightnessDown,       spawn,	     {.v = light_down}},
+    // ### adding volume and brightness keys in sxhkd to be independent form any window manager || function defined above ### //
+    //{0,                    XF86XK_AudioLowerVolume,        spawn,        {.v = downvol}},
+	//{0,                    XF86XK_AudioMute,               spawn,        {.v = mutevol }},
+	//{0,                    XF86XK_AudioRaiseVolume,        spawn,        {.v = upvol}},
+	//{0,                    XF86XK_MonBrightnessUp,         spawn,	     {.v = light_up}},
+	//{0,                    XF86XK_MonBrightnessDown,       spawn,	     {.v = light_down}},
 
     // screenshot fullscreen and cropped
     //{ MODKEY|ControlMask,              XK_s,             spawn,         SHCMD("maim | xclip -selection clipboard -t image/png")},
