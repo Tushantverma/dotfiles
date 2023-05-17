533e928eda29e67ab96bd0a9b7ce663b8c8adcd8
Tushantverma <Tushantverma@users.noreply.github.com>
2023-05-17 17:20:05 +0530

    added separate mic script for dwmblock

--- private_dot_config/tv-dwm/dwmblocks/config.c
+++ private_dot_config/tv-dwm/dwmblocks/config.c
@@ -8,6 +8,7 @@ Block blocks[] = {
     {SCRIPTS_PATH "sb-traffic"       ,5     ,7 },
     {SCRIPTS_PATH "sb-battery"       ,60    ,6 },
     {SCRIPTS_PATH "sb-brightness"    ,0     ,5 },
+    {SCRIPTS_PATH "sb-mic"           ,0     ,8 },
     {SCRIPTS_PATH "sb-volume"        ,0     ,4 },
     {SCRIPTS_PATH "sb-cpu"           ,5     ,3 },
     {SCRIPTS_PATH "sb-memory"        ,5     ,2 },
