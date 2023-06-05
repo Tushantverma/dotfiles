64d2da2a95680bf9ae20c8f2b099f0e9c779503e
Tushantverma <Tushantverma@users.noreply.github.com>
2023-06-06 01:49:23 +0530

    added script to install virtualbox

---
+++ dot_myscripts/executable_6_install_VirtualBox.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+echo "###########################################################################"
+echo "##      Check which kernal is installed            					   ##"
+echo "###########################################################################"
+
+kernel=$(uname -r)
+
+case $kernel in
+    *-lts*)
+        echo "Linux LTS kernel is installed."
+        installpkg="linux-lts-headers"
+        ;;
+    *-hardened*)
+        echo "Linux Hardened kernel is installed."
+        installpkg="linux-hardened-headers"
+        ;;
+    *-zen*)
+        echo "Linux Zen kernel is installed."
+        installpkg="linux-zen-headers"
+        ;;
+    *)
+        echo "Standard Linux kernel is installed."
+        installpkg="linux-headers"
+        ;;
+esac
+
+
+echo "###########################################################################"
+echo "##      This script will install virtualbox       					   ##"
+echo "###########################################################################"
+
+sudo pacman -Syyy
+sudo pacman -S --noconfirm --needed $installpkg
+sudo pacman -S --noconfirm --needed virtualbox
+sudo pacman -S --needed virtualbox-host-dkms
+
+echo "###########################################################################"
+echo "##      Removing all the messages virtualbox produces                    ##"
+echo "###########################################################################"
+VBoxManage setextradata global GUI/SuppressMessages "all"
+
+
+echo "###########################################################################"
+echo "#########               You have to reboot.                       #########"
+echo "###########################################################################"
+
+
+## source
+## https://github.com/erikdubois/arcolinux-nemesis/tree/master/AUR
+## combined multiple virtual box install script into one main
