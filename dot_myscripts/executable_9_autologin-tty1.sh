89a34538f4c61d2f47836062d101701935ce15d1
Tushantverma <Tushantverma@users.noreply.github.com>
2023-09-05 06:27:36 +0530

    some fixes in auto-login

--- dot_myscripts/executable_9_autologin-tty1.sh
+++ dot_myscripts/executable_9_autologin-tty1.sh
@@ -8,12 +8,12 @@ directoryy="/etc/systemd/system/getty@tty1.service.d"
 configure_autologin() {
 
   # if directory dose not exist then create
-  [ ! -e "$directoryy" ] && sudo mkdir -p $directoryy || { echo "$(tput setaf 1)auto login already configured$(tput sgr0)" ; exit 1 ; }
+  [ ! -e "$directoryy/autologin.conf" ] && sudo mkdir -p $directoryy || { echo "$(tput setaf 1)auto login already configured$(tput sgr0)" ; exit 1 ; }
 
-  read -p "Enter Username (Enter nothing for Current User): " username
+  read -p "Enter Username (Enter for Default $(whoami)): " username
 
   # if empty input then assing current username if non empty then assing entered username
-  [ -z "$username" ] && { current_user=$(whoami) ; echo "Current user: $current_user" ; } || { current_user=$(username) ; echo "You entered: $username" ; }
+  [ -z "$username" ] && { current_user=$(whoami) ; echo "Current user: $current_user" ; } || { current_user=$(username) ; echo "You entered: $current_user" ; }
 
 
   echo "
@@ -23,7 +23,7 @@ configure_autologin() {
 
   # sudo systemctl enable getty@tty1.service (it always in running)
 
-  echo "Auto-login for user '$current_user' has been configured."
+  echo "$(tput setaf 2)Auto-login for user '$current_user' has been configured.$(tput sgr0)"
 
 }
 
@@ -38,7 +38,7 @@ remove_autologin() {
   [ -e "$directoryy" ] && sudo rm -rf $directoryy || { echo "$(tput setaf 1)auto login already removed$(tput sgr0)" ; exit 1 ; }
 
   # sudo systemctl disable getty@tty1.service (it always in running)
-  echo "Auto-login configuration has been removed."
+  echo "$(tput setaf 2)Auto-login configuration has been removed.$(tput sgr0)"
 
 }
 
