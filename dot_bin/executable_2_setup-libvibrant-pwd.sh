be248258dde0a70afdcf9c65b75f6394713ed373
TushantVerma <Tushantverma@users.noreply.github.com>
2023-10-12 17:15:00 +0530

    improved the script libvibrant and xbanish

--- dot_bin/executable_2_setup-libvibrant-pwd.sh
+++ dot_bin/executable_2_setup-libvibrant-pwd.sh
@@ -1,34 +1,39 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# cd into the directory first where you want to place this script files
+# ---------------------------------------------- checkup the internet connection --------------------------------------- #
+# wget -q --spider https://www.google.com || { echo "No internet connection"; exit 1; }  # wget just checks web page exist or not without downloading
+# wget -q --spider https://www.google.com && { echo "Internet is working" ; } || { echo "No internet connection"; exit 1; }
+wget -q --spider https://www.google.com || wget -q --spider https://www.apple.com || { echo "No internet connection"; exit 1; } && echo "Internet is working" 
+# curl -s --head --fail https://www.google.com > /dev/null && { echo "Internet is working" ; } || { echo "No internet connection"; exit 1; }  # it downloads the head code to check
 
-### install all dependency
-sudo pacman -Sy --noconfirm --needed libx11 libxrandr libxnvctrl cmake
+### install all dependency ###
+sudo pacman -Sy --noconfirm --needed libx11 libxrandr cmake
 # libxnvctrl ==> not installing right now this is for nvedia
 
+[ -d "$HOME/.config/libvibrant" ] && rm -rf "$HOME/.config/libvibrant/"
+git clone --depth 1 https://github.com/libvibrant/libvibrant.git "$HOME/.config/libvibrant/"
+rm -rf "$HOME/.config/libvibrant/.git*"
+mkdir "$HOME/.config/libvibrant/build/"
 
-cd ~/.config/
+# Run CMake to configure the project
+cmake -B "$HOME/.config/libvibrant/build/" -S "$HOME/.config/libvibrant/"
+# The -B flag is used to specify the directory where CMake should generate build system files
+# The -S flag is used to specify the directory containing the CMake project's CMakeLists.txt file
+# you can also use ($cmake ..) in your build directory
 
-# delete old file is there is any
-rm -rf libvibrant
+# Build the project
+make -C "$HOME/.config/libvibrant/build/"
 
-git clone --depth 1 https://github.com/libvibrant/libvibrant.git
+echo "adding this command into the startup script"
+display=$(xrandr --listmonitors | awk 'NR==2{print $NF}')  # getting the display name
+# eDP-1 is your display port name got it form xrandr
 
-cd libvibrant
-rm -rf .git*
-mkdir build
-cd build
-cmake ..
-make
-
-echo "add this command into the startup script"
-
-# getting the display name
-display=$(xrandr --listmonitors | awk 'NR==2{print $NF}')
-echo "$PWD/vibrant-cli/vibrant-cli $display 1.6" >> ~/.startup.sh
-# eDP-1 is your display port name find it form xrandr
-
+## if the line already exist in the startup script dont add it again
+if ! grep -qF "\$HOME/.config/libvibrant/build/vibrant-cli/vibrant-cli $display 1.6"    "$HOME/.config/startup.sh" 2>/dev/null; then
+         echo "\$HOME/.config/libvibrant/build/vibrant-cli/vibrant-cli $display 1.6" >> "$HOME/.config/startup.sh"
+fi
 
 
 
 # source :- https://github.com/libvibrant/libvibrant/
+
--- dot_bin/executable_3_xbanish-hideCursor.sh
+++ dot_bin/executable_3_xbanish-hideCursor.sh
@@ -1,13 +1,23 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-git clone --depth=1 https://github.com/jcs/xbanish.git
-cd xbanish/
-sudo make; sudo make install; sudo make clean;
-cd ..
-rm -rf xbanish/
-echo "xbanish &" >> ~/.startup.sh
+# ---------------------------------------------- checkup the internet connection --------------------------------------- #
+# wget -q --spider https://www.google.com || { echo "No internet connection"; exit 1; }  # wget just checks web page exist or not without downloading
+# wget -q --spider https://www.google.com && { echo "Internet is working" ; } || { echo "No internet connection"; exit 1; }
+wget -q --spider https://www.google.com || wget -q --spider https://www.apple.com || { echo "No internet connection"; exit 1; } && echo "Internet is working" 
+# curl -s --head --fail https://www.google.com > /dev/null && { echo "Internet is working" ; } || { echo "No internet connection"; exit 1; }  # it downloads the head code to check
+
+git clone --depth 1 https://github.com/jcs/xbanish.git /tmp/xbanish/
+
+sudo make         -C /tmp/xbanish/
+sudo make install -C /tmp/xbanish/
+sudo make clean   -C /tmp/xbanish/
+
+rm -rf /tmp/xbanish/
+
+# Only add the name 'xbanish &' if it does not already exist in the file.
+grep -qF "xbanish &" "$HOME/.config/startup.sh" 2>/dev/null || echo "xbanish &" >> "$HOME/.config/startup.sh"
 
 ## how to remove it 
-#1 remove form ~/.startup.sh
+#1 remove form "$HOME/.config/startup.sh"
 #2 sudo rm -rf /usr/local/bin/xbanish (remove binary)
 #3 sudo rm -rf /usr/local/man/man1/xbanish.1 (remove manpage)
