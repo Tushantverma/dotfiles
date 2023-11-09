57eec10fab8669488e3600b9bbe4a1ee2f5d86f8
Tushantverma <Tushantverma@users.noreply.github.com>
2023-11-10 04:24:00 +0530

    updated auto_boot_grub_script

--- dot_bin/executable_11_grub-auto-boot.sh
+++ dot_bin/executable_11_grub-auto-boot.sh
@@ -3,14 +3,12 @@
 # run the whole script as root 
 [ "$EUID" -ne 0 ] && echo "This script requires root privileges." && exec sudo sh "$0" "$@"; 
 
-read -rep "$(tput setaf 3)show grub on boot : (yes / no): $(tput sgr0)" choice
+read -rep "$(tput setaf 3)Grub on boot : [ (e)nable / (d)isable ]: $(tput sgr0)" choice
 
 case $choice in
-    y | yes )   sed -i 's/GRUB_TIMEOUT=.*$/GRUB_TIMEOUT=5/' /etc/default/grub ;;
-    n | no  )   sed -i 's/GRUB_TIMEOUT=.*$/GRUB_TIMEOUT=0/' /etc/default/grub ;;
-    *)          echo "$(tput setaf 1)Invalid choice. Please enter either 1 or 2.$(tput sgr0)" ; exit 1 ;;
+    e | enable )   sed -i 's/GRUB_TIMEOUT=.*$/GRUB_TIMEOUT=5/' /etc/default/grub && echo "$(tput setaf 2)Enabled $(tput sgr0)";;
+    d | disable)   sed -i 's/GRUB_TIMEOUT=.*$/GRUB_TIMEOUT=0/' /etc/default/grub && echo "$(tput setaf 2)Disabled$(tput sgr0)";;
+    *)  echo "$(tput setaf 1)Invalid choice. Please enter [(e)nable / (d)isable] $(tput sgr0)" ; exit 1 ;;
 esac
 
 grub-mkconfig -o /boot/grub/grub.cfg
-
-echo "$(tput setaf 2)All Done$(tput sgr0)"
