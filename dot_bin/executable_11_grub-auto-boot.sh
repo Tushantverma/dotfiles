7e0543cffa780879d8d23744ccc24b719e1ba0d8
Tushantverma <Tushantverma@users.noreply.github.com>
2023-11-11 05:08:00 +0530

    some fixes in grub_auto_login_script

--- dot_bin/executable_11_grub-auto-boot.sh
+++ dot_bin/executable_11_grub-auto-boot.sh
@@ -8,7 +8,9 @@ read -rep "$(tput setaf 3)Grub on boot : [ (e)nable / (d)isable ]: $(tput sgr0)"
 case $choice in
     e | enable )   sed -i 's/GRUB_TIMEOUT=.*$/GRUB_TIMEOUT=5/' /etc/default/grub && echo "$(tput setaf 2)Enabled $(tput sgr0)";;
     d | disable)   sed -i 's/GRUB_TIMEOUT=.*$/GRUB_TIMEOUT=0/' /etc/default/grub && echo "$(tput setaf 2)Disabled$(tput sgr0)";;
-    *)  echo "$(tput setaf 1)Invalid choice. Please enter [(e)nable / (d)isable] $(tput sgr0)" ; exit 1 ;;
+    *)  echo "$(tput setaf 1)Invalid choice. Please enter [ (e)nable / (d)isable ] $(tput sgr0)" ; exit 1 ;;
 esac
 
 grub-mkconfig -o /boot/grub/grub.cfg
+
+echo "$(tput setaf 2)#--------------SCRIPT_ENDED--------------#$(tput sgr0)"
