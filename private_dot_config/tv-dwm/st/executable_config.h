2e3fdb7e90d90abd62039040f850d8f9e05a01a5
TushantVerma <TushantVerma@users.noreply.github.com>
2024-02-13 14:23:00 +0530

    changed the cursor shape from block to beam

--- private_dot_config/tv-dwm/st/executable_config.h
+++ private_dot_config/tv-dwm/st/executable_config.h
@@ -156,7 +156,7 @@ unsigned int background = 258;
  * 6: Bar ("|")
  * 7: Snowman ("☃")
  */
-static unsigned int cursorshape = 2;
+static unsigned int cursorshape = 6;
 
 /*
  * Default columns and rows numbers
