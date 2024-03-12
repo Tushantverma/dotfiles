d6ffb40f29f023c3ef8dcee06b441148c475a398
TushantVerma <TushantVerma@users.noreply.github.com>
2024-03-12 17:14:00 +0530

    added variable in 1_setup_all.sh

--- dot_bin/executable_1_setup_all.sh
+++ dot_bin/executable_1_setup_all.sh
@@ -2,23 +2,23 @@
 
 # my OS install script will use '*'
 
-echo "my home is $HOME"
-echo "my user is $USER"
-
+echo "my User is : $USER"
+echo "my Home is : $HOME"
+ 
 # building dwm
-cd /home/*/.config/tv-dwm/chadwm/
+cd /home/$USER/.config/tv-dwm/chadwm/
 sudo make
 sudo make install
 sudo make clean
 
 # building st
-cd /home/*/.config/tv-dwm/st/
+cd /home/$USER/.config/tv-dwm/st/
 sudo make
 sudo make install
 sudo make clean
 
 # building dwmblocks
-cd /home/*/.config/tv-dwm/dwmblocks/
+cd /home/$USER/.config/tv-dwm/dwmblocks/
 sudo make
 sudo make install
 sudo make clean
